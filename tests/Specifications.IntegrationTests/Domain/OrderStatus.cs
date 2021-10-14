998c448a558d0ac5399ee72e5070ecc95de8f65f
Ivan Filatov <i.filatov@foodplexgroup.ru>
2021-10-15 01:13:35 +0500

    tests: add IntegrationTests Sqlite + EF Core Assert specification where conditions in sql query

---
+++ tests/Specifications.IntegrationTests/Domain/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Specifications.IntegrationTests.Domain
+{
+    public enum OrderStatus
+    {
+        created,
+        accepted,
+        cooked,
+        packed,
+        delivered,
+        completed,
+        rejected
+    }
+}
---
+++ tests/Specifications.IntegrationTests/ResetDatabaseAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Specifications.IntegrationTests.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Specifications.IntegrationTests
+{
+    public class ResetDatabaseAttribute : BeforeAfterTestAttribute
+    {
+        public override void After(MethodInfo methodUnderTest)
+        {
+            using var db = new OrdersDbContext();
+            db.Database.EnsureDeleted();
+
+            base.After(methodUnderTest);
+        }
+    }
+}
